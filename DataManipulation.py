d8128bc149a56551c1b746661091d63bcbef8826
Krishna Chaitanya Dodda <dkc-bitsian@users.noreply.github.com>
2016-11-27 17:48:36 -0500

    Update DataManipulation.py

--- DataManipulation.py
+++ DataManipulation.py
@@ -2,14 +2,14 @@ import csv
 
 mycsv = csv.reader(open('./combined.csv'))
 writer=csv.writer(open('./ratings.csv','wb'))
-writer.writerow(['UserID','MovieID','Rating'])
+writer.writerow(['UserID','JokeID','Rating'])
 for row in mycsv:
     user=row[0]
     count=1
     for rating in row[1:]:
         list=[]
         list.append(user)#userID
-        list.append(count)#movieID
+        list.append(count)#jokeID
         list.append(rating)
         writer.writerow(list)
         count+=1
